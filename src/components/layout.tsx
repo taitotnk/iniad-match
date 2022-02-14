f0421bb871a971fe2eccc34de4f34ed9eae3296e
Taito-Code <taito.tanaka1211@gmail.com>
2022-02-15 00:38:14 +0900

    Feature: update layout component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { ReactNode } from "react";
+import Link from "next/link";
 
 type Props = {
   children: ReactNode;
@@ -11,12 +12,30 @@ const Layout = ({ children, title }: Props) => (
     <Head>
       <title>{title}</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@taito_1211" />
+      <meta
+        property="og:image"
+        content="https://iniad-match.vercel.app/iniad-match-ogp.jpg"
+      />
+      <meta
+        property="twitter:image"
+        content="https://iniad-match.vercel.app/iniad-match-ogp.jpg"
+      />
     </Head>
-    <header></header>
+    <header>
+      <div className="container mx-auto">
+        <Link href="/">
+          <a>
+            <h1 className="text-white pl-10">INIAD-MATCH 👋</h1>
+          </a>
+        </Link>
+      </div>
+    </header>
     <div>{children}</div>
-    <footer></footer>
+    <footer>
+      <div className="text-white text-center pl-10">
+        © 2021 Copyright INIAD-MATCH
+      </div>
+    </footer>
   </>
 );
 
